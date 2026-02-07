bb05abd7b8951b2868309808bdcaf2914ffd670b
Oleg Razumov <orazumoff@gmail.com>
2026-02-07 17:24:43 +0300

    Update SearchTests.java

--- SearchTests.java
+++ SearchTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://https://dzen.ru//");
+        open("https://dzen.ru//");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
